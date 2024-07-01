fafbd4559166054cc44cee0b8531b70b1d648e03
ksmnhb <162869110+ksmnhb@users.noreply.github.com>
2024-07-01 17:04:58 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>課題５_テストの平均点</title>
+    </head>
+
+    <body>
+ 
+        <p>
+            <?php
+            // 変数に値を代入
+            $score1 = 80;
+            $score2 = 60;
+            $score3 = 55;
+            $score4 = 40;
+            $score5 = 100;
+            $score6 = 25;
+            $score7 = 80;
+            $score8 = 95;
+            $score9 = 30;
+            $score10 = 60;
+            
+            $total = $score1 + $score10 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9;
+
+
+            // 変数と数値を計算し、その結果を出力
+            echo $total / 10;
+
+            
+            
+            ?>
+        </p>
+    </body>
+</html>
