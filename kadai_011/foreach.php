1a043300457db4743d243f83f2f794ccb4ef0682
ksmnhb <162869110+ksmnhb@users.noreply.github.com>
2024-07-05 16:52:42 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>課題１１</title>
+    </head>
+    <body>
+    
+        <p>
+            <?php
+            $product_data= ["名前" => "玉ねぎ", "値段" => 200,"産地" =>"北海道"];
+            
+            // 連想配列$personal_dataのキーと値を1つずつ順番に
+            foreach($product_data as $key => $value){
+                echo "{$key}：{$value}<br>";
+            }
+
+        
+            
+            ?>
+        </p>
+       
+        
+
+
+    </body>
+</html>
