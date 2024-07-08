6facebf869578d2b6715a412488d5a948bcd5eb0
ksmnhb <162869110+ksmnhb@users.noreply.github.com>
2024-07-08 12:24:13 +0900

    Add files via upload

---
+++ kadai_web_app/complete.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+        <meta charset="UTF-8">
+        <title>課題WEBアプリ</title>
+</head>
+<body>
+    <h1>登録が完了しました。</h1>
+    <p>社員情報がデータベースに保存されました。</p>
+    <button id="back-btn" onclick="location.href='form.php';">戻る</button>
+</html>
